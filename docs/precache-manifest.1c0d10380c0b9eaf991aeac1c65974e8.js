baeab224ff2ebcdc6e7647ad20eaf0700c64ceec
luciaherrrador <luciaherradorcarrasco@gmail.com>
2020-03-23 09:30:49 +0100

    add docs folder

---
+++ docs/precache-manifest.1c0d10380c0b9eaf991aeac1c65974e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a676f6738658d8beb202b124d6dbdcd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f23378560f75f38c77b",
+    "url": "./static/css/main.133d4644.chunk.css"
+  },
+  {
+    "revision": "d99c7ecbef49697eb1b7",
+    "url": "./static/js/2.363914ff.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.363914ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f23378560f75f38c77b",
+    "url": "./static/js/main.4adac80e.chunk.js"
+  },
+  {
+    "revision": "0352349386b48ddf34a0",
+    "url": "./static/js/runtime-main.15ac2d79.js"
+  },
+  {
+    "revision": "195898bfd9e3eff6206565f427f00d36",
+    "url": "./static/media/oveja.195898bf.jpg"
+  },
+  {
+    "revision": "f08928523bf3a8e8ac5c266a1e49744c",
+    "url": "./static/media/prado.f0892852.jpg"
+  }
+]);
